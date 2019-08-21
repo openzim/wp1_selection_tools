10ac628eb66d646908c14aa5924e539bdd6afabb
Kelson <kelson@kiwix.org>
2019-08-21 10:24:32 +0200

    Allow to custom biggest wikipedia lower limit

--- build_all_selections.sh
+++ build_all_selections.sh
@@ -4,7 +4,7 @@
 set -e
 set -o pipefail
 
-for LANG in `./build_biggest_wikipedia_list.sh | cut -d " " -f1`
+for LANG in `./build_biggest_wikipedia_list.sh 0 | cut -d " " -f1`
 do
     ./build_selections.sh $LANG || exit $?
 done
--- build_biggest_wikipedia_list.sh
+++ build_biggest_wikipedia_list.sh
@@ -4,4 +4,16 @@
 set -e
 set -o pipefail
 
-curl -s "http://wikistats.wmflabs.org/api.php?action=dump&table=wikipedias&format=csv" | cut -d',' -f3,4 | tr ',' ' ' | sed  '1d' | sort -k 2,2 -n -r | perl -ne '($lang, $count) = split(" ", $_); if ($count > 500000) { print "$lang $count\n" }'
+# Get MIN_ARTICLE_COUNT
+int_re='^[0-9]+$'
+if [[ "$1" =~ $int_re ]]
+then
+    MIN_ARTICLE_COUNT=$1
+    >&2 echo "Get only Wikipedias with more than $MIN_ARTICLE_COUNT articles"
+else
+    >&2 echo "Give the minimal article of article in the Wikipedias"
+    exit 1
+fi
+
+# Request wikistats
+curl -s "http://wikistats.wmflabs.org/api.php?action=dump&table=wikipedias&format=csv" | cut -d',' -f3,4 | tr ',' ' ' | sed  '1d' | sort -k 2,2 -n -r | perl -ne "(\$lang, \$count) = split(' ', \$_); if (\$count > $MIN_ARTICLE_COUNT) { print \"\$lang \$count\n\" }"
