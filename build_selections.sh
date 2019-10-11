5e5283f112268eb70b6e42d603846c47739e58d5
Emmanuel Engelhart <kelson@kiwix.org>
2019-10-11 09:23:47 +0100

    Include medicine translate welcome pages

--- build_selections.sh
+++ build_selections.sh
@@ -318,6 +318,8 @@ then
     cp $DIR/langlinks $EN_NEEDED
 else
     grep -P "\t$WIKI_LANG\t" $EN_NEEDED/langlinks > $WIKI_LANGLINKS
+    grep -P "\t$WIKI_LANG\t" $EN_NEEDED/medicine.langlinks > $WIKI_LANGLINKS
+    sort -u -o $WIKI_LANGLINKS $WIKI_LANGLINKS
     rm -rf $DIR/projects
     mkdir $DIR/projects
     for FILE in `find $EN_NEEDED/projects/ -type f`
