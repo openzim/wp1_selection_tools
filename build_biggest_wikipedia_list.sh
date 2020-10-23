239a695926803538cd4f0b6573a11fe35886735a
Emmanuel Engelhart <kelson@kiwix.org>
2020-10-23 13:36:03 +0200

    Wikistats has moved

--- build_biggest_wikipedia_list.sh
+++ build_biggest_wikipedia_list.sh
@@ -16,4 +16,4 @@ else
 fi
 
 # Request wikistats
-curl -s "http://wikistats.wmflabs.org/api.php?action=dump&table=wikipedias&format=csv" | cut -d',' -f3,4 | tr ',' ' ' | sed  '1d' | sort -k 2,2 -n -r | perl -ne "(\$lang, \$count) = split(' ', \$_); if (\$count > $MIN_ARTICLE_COUNT) { print \"\$lang \$count\n\" }"
+curl -s "https://wikistats.wmcloud.org/api.php?action=dump&table=wikipedias&format=csv" | cut -d',' -f3,4 | tr ',' ' ' | sed  '1d' | sort -k 2,2 -n -r | perl -ne "(\$lang, \$count) = split(' ', \$_); if (\$count > $MIN_ARTICLE_COUNT) { print \"\$lang \$count\n\" }"
